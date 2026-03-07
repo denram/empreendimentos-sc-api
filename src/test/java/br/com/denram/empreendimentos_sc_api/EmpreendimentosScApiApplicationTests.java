82522019033772acc90ef6da142f901b0c1853f5
denis-ramos_senior <denis.ramos@senior.com.br>
2026-03-07 18:09:37 -0300

    feat: estrutura inicial do projeto Spring Boot (#3)

---
+++ src/test/java/br/com/denram/empreendimentos_sc_api/EmpreendimentosScApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.denram.empreendimentos_sc_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpreendimentosScApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
